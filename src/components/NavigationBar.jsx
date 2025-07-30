6aa893345603fbd03728878c115037b0bc77bf55
Luis <luis.villela021@gmail.com>
2025-07-30 17:42:18 -0500

    all caps looks better?

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,10 +3,10 @@ import roboticsLogo from '/img/croboticslogo.jpg';
 
 export default function NavBar() {
     return (
-        <nav className="relative flex items-center justify-around w-full h-16 m-0 gap-36 shadow-2xl">
-            <a href="/" className="flex items-center gap-4">
+        <nav className="relative flex items-stretch justify-around w-full h-16 m-0 gap-36 shadow-2xl">
+            <a href="/" className="flex items-center gap-4 text-white hover:text-blue-500 will-change-[filter] transition-[filter_colors] duration-200 hover:drop-shadow-[0_0_1em_#503cff66]">
                 <img src={ roboticsLogo } alt="" className="h-10 w-10 border-2 rounded-full" />
-                <h1 className="text-white text-3xl font-bold">CHS Robotics</h1>
+                <h1 className="text-inherit text-3xl font-bold">CHS ROBOTICS</h1>
             </a>
             <div className="links flex gap-8">
                 <a href="#about-us">About Us</a>
