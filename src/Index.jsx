39d61788ce7d10bec4bab5a0f179414da643c3c3
Luis <luis.villela021@gmail.com>
2025-07-31 15:32:18 -0500

    small fixes on index page

--- src/Index.jsx
+++ src/Index.jsx
@@ -18,10 +18,10 @@ app.render(
 
         <div className="flex flex-col items-center my-6">
             <a href="#" className="logo">
-                <img src={ robotLogo } className="h-48 p-1 rounded-full will-change-[filter] transition-[filter] duration-200 hover:drop-shadow-[0_0_2em_#503cff66]" id="roboticsLogo" alt="CHS Robotics Logo" />
+                <img src={ robotLogo } className="h-48 p-1 rounded-full will-change-[filter] transition-[filter] duration-200" id="roboticsLogo" alt="CHS Robotics Logo" />
             </a>
-            <h1 className="text-8xl max-lg:text-6xl font-extrabold my-4 text-transparent max-lg:text-white bg-gradient-to-r from-blue-50 from-20% to-blue-400 bg-clip-text bg-size-[300%_auto] bg-position-[0%] hover:bg-position-[60%] transition-all duration-300">CHS ROBOTICS CLUB</h1>
-            <h2 className="text-3xl tracking-wider font-medium">Innovate. Build. Compete. Inspire. Idk.</h2>
+            <h1 className="m-4 text-8xl max-lg:text-6xl font-extrabold text-transparent max-lg:text-white bg-gradient-to-r from-blue-50 from-20% to-blue-400 bg-clip-text bg-size-[300%_auto] bg-position-[0%] hover:bg-position-[60%] transition-all duration-300">CHS ROBOTICS CLUB</h1>
+            <h2 className="mx-4 text-3xl tracking-wider font-medium">Innovate. Build. Compete. Inspire. Idk.</h2>
         </div>
 
         <div className="section flex justify-around max-w-full">
@@ -31,13 +31,13 @@ app.render(
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa itaque necessitatibus accusamus atque neque dicta vero mollitia aliquid omnis obcaecati rem nesciunt perspiciatis dignissimos facilis placeat recusandae in animi, voluptatum sit quos! Facilis laudantium doloribus nihil repellat sunt tempora, harum modi deleniti eaque consectetur praesentium voluptas vel delectus accusamus distinctio repellendus officiis repudiandae expedita non inventore sit. Sed ullam quam deserunt eveniet molestias rerum deleniti a quidem voluptatem sequi odio iste temporibus, non repellendus maxime. Accusantium soluta iste reprehenderit optio consequatur inventore saepe odit molestias non, ipsam facere sapiente, cum, fuga in. Distinctio in explicabo excepturi laudantium assumenda voluptatem dolor!
                 </p>
             </div>
-            <div id="ig-embed" className="side m-8 max-w-1/3 max-h-full hover:rotate-1">
-                <iframe className="rounded-2xl h-full" src="https://www.instagram.com/cchavezroboticsclub/embed/?cr=1&v=14&rd=http%3A%2F%2Flocalhost%3A5173&rp=%2Frobotics-website%2F#%7B%22ci%22%3A0%2C%22os%22%3A107.90000000596046%2C%22ls%22%3A14.5%2C%22le%22%3A73.09999999403954%7D" frameborder="0"></iframe>
+            <div id="ig-embed" className="side m-8 mr-16 max-w-1/3 max-h-full hover:rotate-1">
+                <iframe className="rounded-2xl h-full bg-white" src="https://www.instagram.com/cchavezroboticsclub/embed/"></iframe>
             </div>
         </div>
 
         <div className="section flex justify-around">
-            <div className="side m-8 max-w-1/3 self-center hover:-rotate-1">
+            <div className="side m-8 ml-16 max-w-1/3 self-center hover:-rotate-1">
                 <img src={ dremelImage } alt="Mr. Perez using a dremel." className="h-full w-fit rounded-2xl" />
             </div>
             <div className="my-8 ml-auto px-16 max-lg:px-4 max-w-2/3 text-right font-normal">
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import roboticsLogo from '/img/robotlogo.jpg';
+import '/src/style.css';
 
 export default function NavBar() {
     return (
-        <nav className="relative flex items-stretch justify-around w-full h-16 m-0 gap-36 shadow-2xl">
+        <nav className="sticky top-0 z-1 flex items-stretch justify-around w-full h-16 m-0 gap-36 backdrop-blur-lg shadow-2xl">
             <a href="/" className="flex items-center gap-4 text-white hover:text-blue-500 will-change-[filter] transition-[filter_colors] duration-200 hover:drop-shadow-[0_0_1em_#503cff66]">
                 <img src={ roboticsLogo } alt="" className="h-10 w-10 border-2 rounded-full" />
                 <h1 className="text-inherit text-3xl font-bold">CHS ROBOTICS</h1>
