39b426c65aa16e75cb30ef277d408a78006abc6e
Luis <luis.villela021@gmail.com>
2025-07-31 22:23:21 -0500

    fixing navbar style

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TeamMember({ name, role, image, description }) {
     return (
-        <div className="flex flex-col items-center max-w-96 p-4 bg-gray-100/10 rounded-xl shadow-md">
+        <div className="flex flex-col items-center max-w-96 p-4 rounded-xl bg-gray-100/10 border-gray-50/20 border-1 transition-transform hover:scale-105 shadow-md">
             <img src={image} alt="" className="w-32 h-32 rounded-full border-2 mb-4" />
             <h3 className="text-2xl font-semibold">{name}</h3>
             <p className="text-lg text-gray-300">{role}</p>
